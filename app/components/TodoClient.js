af81522d41ea555daf300b3f15883171ed086a86
gabriel note <gabriel.emil.note@gmail.com>
2026-02-04 14:49:59 +0100

    Flyttat logik till en ny fil (api_calls). lagt till setTask("") för att rensa fältet efter att man skrivit in en ny task

--- app/components/TodoClient.js
+++ app/components/TodoClient.js
@@ -3,37 +3,17 @@
 import { useState } from "react";
 import TodoList from "./TodoList";
 import Image from "next/image";
+import createTask from "../api/api_calls"
 
 export default function TodoClient({todoClientData}) {
-    const [showTodos, setShowTodos] = useState(false);
+    const [showTodos, setShowTodos] = useState(true);
     const [todos, setTodos] = useState(todoClientData);
     const [task, setTask] = useState("");
 
     const handleCreateTask = async (e) => {
     e.preventDefault();
-
-      try {
-        const response = await fetch("http://localhost:8080/tasks", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            task: task
-          }),
-        });
-
-        if (!response.ok) throw new Error("Failed to create");
-
-        const newTask = await response.json();
-
-        // Update state with new task
-        setTodos([...todos, newTask]);
-      }
-      catch (error) {
-        console.error("Error:", error);
-        alert("Failed to create task");
-      }
+    createTask(task);
+    setTask("");
     }
 
   return (
@@ -44,9 +24,10 @@ export default function TodoClient({todoClientData}) {
       <form onSubmit={handleCreateTask} className="mb-4 space-y-2">
         <input
           type="text"
-          placeholder="Enter task"
+          placeholder="Enter task here"
           value={task}
           onChange={(e) => setTask(e.target.value)}
+          
           required
           className="w-full px-3 py-2 border rounded"
         />
