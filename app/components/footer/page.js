533a05628ceca61d3029e746c6511d57d8f6534c
LialviSWE <alexbranderlund@gmail.com>
2026-04-23 13:16:23 +0200

    Changed back to .js, made some big but important changes

--- app/components/footer/page.js
+++ app/components/footer/page.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="border-t mt-8 p-4 text-left fixed bottom-0 w-full bg-blue-950">
       <p>© {new Date().getFullYear()} The Greatest To Do List That Ever Was™</p>
     </footer>
   );
